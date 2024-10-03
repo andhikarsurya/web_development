76c45802aa2a116bf3e3dd0d1793e36096b698ff
andhi <andhikarsurya@gmail.com>
2024-10-03 11:48:39 +0700

    test

--- exercise_3/number_4.ts
+++ exercise_3/number_4.ts
@@ -1,4 +1,21 @@
-namespace soal{
-    let num:number = 1000;
-    console.log(`Rp.${num},00`)
+// namespace soal_a{
+//     let num:number = 1000;
+//     console.log(`Rp.${num},00`)
+// }
+
+// namespace soal_b{
+//     let num:number = 1000;
+//     let format = new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(num);
+// console.log(format);
+// }
+
+namespace soal_c{
+let num = 10000;
+let numStr = num.toString();
+let format = numStr.slice(-3);
+if (numStr.length > 3) {
+    format = numStr.slice(0, -3) + '.' + format;
+}
+let finalnum = "Rp." + format + ",00";
+console.log(finalnum);
 }
