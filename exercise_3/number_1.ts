e83e1f3073901f7057b2bfcd9533f37ef82761e2
andhi <andhikarsurya@gmail.com>
2024-10-03 13:46:57 +0700

    nice

--- exercise_3/number_1.ts
+++ exercise_3/number_1.ts
@@ -1,6 +1,6 @@
 namespace soal{
     let num: number = 9;
-    for (let index = 1; index <= num+1; index++) {
+    for (let index = 1; index <= 10; index++) {
         console.log(`${num} x ${index} = ${num*index}`);
         
     }
